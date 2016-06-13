7e45dad8366112ca7d987fc3d3c31ca7c6759597
Toby Wilkins <tobywilkins@Tobys-MacBook-Pro-2.local>
2016-06-13 17:24:10 +0100

    got feature test working

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'sinatra/base'
 require 'data_mapper'
+require './models/link'
 
 class BookmarkManager < Sinatra::Base
   get '/' do
     'Hello BookmarkManager!'
+  end
+
+  get '/links' do
+    @links = Link.all
+    erb(:links)
   end
 
   # start the server if ruby file executed directly
