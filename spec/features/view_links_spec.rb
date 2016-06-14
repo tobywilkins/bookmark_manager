1af52ac47b739197ddba6f0e66e9872c5d2c8bd4
Alex-Swann <alex_swann@hotmail.co.uk>
2016-06-14 09:54:33 +0100

    added 1st feature test

--- spec/features/view_links_spec.rb
+++ spec/features/view_links_spec.rb
@@ -1,6 +1,12 @@
 
 feature 'View Links' do
   scenario 'shows a list of links on the homepage'do
-    expect(page).to have_selector(:link_or_button, 'Visit Page')
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit '/links'
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).to have_content('Makers Academy')
+    end
   end
 end
