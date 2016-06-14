0b7cb7498103ab213ee4a07887b6ead61c380c60
Alex-Swann <alex_swann@hotmail.co.uk>
2016-06-14 10:12:11 +0100

    passed first feature test

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
+require './models/link'
 
 class BookMarkM < Sinatra::Base
   get '/' do
     'Hello BookMarkM!'
   end
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
